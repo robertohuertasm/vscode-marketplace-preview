8c6bba443753406d4d65ffb79e1d22d789d327dc
Roberto Huertas <roberto.huertas@outlook.com>
2022-10-18 00:07:09 +0200

    feat(readme): get html from readme file

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,62 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as MarkdownIt from 'markdown-it';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log(
     'Congratulations, your extension "vscode-marketplace-preview" is now active!',
   );
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     'vscode-marketplace-preview.helloWorld',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        'Hello World from vscode-marketplace-preview!',
+    async () => {
+      const outputChannel = vscode.window.createOutputChannel(
+        'Marketplace Preview',
       );
+      await getReadmeAsHtml(outputChannel, context);
     },
   );
 
   context.subscriptions.push(disposable);
+}
+
+async function getReadmeAsHtml(
+  outputChannel: vscode.OutputChannel,
+  context: vscode.ExtensionContext,
+) {
+  const fs = vscode.workspace.fs;
+  const md = new MarkdownIt({ html: true, linkify: true });
+  const root = vscode.workspace.workspaceFolders?.[0].uri;
+
+  const readmeUri = vscode.Uri.joinPath(root!, 'README.md');
+  const readme = await fs.readFile(readmeUri);
+  const readmeStr = md.render(readme.toString());
+
+  const cssPath = vscode.Uri.joinPath(
+    context.extensionUri,
+    'resources/template.css',
+  )
+    .with({ scheme: 'vscode-resource' })
+    .toString();
+
+  const templateUri = vscode.Uri.joinPath(
+    context.extensionUri,
+    'resources/template.html',
+  );
+  const template = await fs.readFile(templateUri);
+  const templateStr = template
+    .toString()
+    .replace('<!-- MARKDOWN-INSERT-->', readmeStr)
+    .replace('{{cssPath}}', cssPath);
+
+  const panel = vscode.window.createWebviewPanel(
+    'preview',
+    'Preview',
+    vscode.ViewColumn.One,
+  );
+  panel.webview.html = templateStr;
+
+  outputChannel.appendLine(readmeStr);
+  // outputChannel.show();
 }
 
 // This method is called when your extension is deactivated
